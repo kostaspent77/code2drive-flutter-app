9f34b3f9ad4cf9f52f7d9bc5a12010638a1cd2f2
kostaspent77 <kostaspent77@gmail.com>
2025-01-04 23:41:03 +0200

    step-2

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,4 @@
-import 'package:code2drive/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -35,73 +33,68 @@ late final TextEditingController _password;
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-                options: DefaultFirebaseOptions.currentPlatform,
-              ),
-
-        builder: (context, snapshot) {
-          switch(snapshot.connectionState) {
-            case ConnectionState.done:
-            return Column(
-                children: [
-                TextField(
-                  controller: _email,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: 
-                    const InputDecoration(
-                    hintText: 'Enter your email here'
-                    ),
-                ),
-                TextField(
-                  controller: _password,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: 
-                    const InputDecoration(
-                    hintText: 'Enter your password here'
-                    ),
-                ),
-                TextButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                          email: email,
-                          password: password,
-                        );
-                        print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'user-not-found') {
-                          print('No user found for that email.');
-                        } else if (e.code == 'wrong-password') {
-                          print('Wrong password provided for that user.');
-                        } else {
-                          print('Unhandled FirebaseAuthException: ${e.code}');
+      body: Column(
+                  children: [
+                  TextField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: 
+                      const InputDecoration(
+                      hintText: 'Enter your email here'
+                      ),
+                  ),
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: 
+                      const InputDecoration(
+                      hintText: 'Enter your password here'
+                      ),
+                  ),
+                  TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        try {
+                          final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                            email: email,
+                            password: password,
+                          );
+                          print(userCredential);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            print('No user found for that email.');
+                          } else if (e.code == 'wrong-password') {
+                            print('Wrong password provided for that user.');
+                          } else {
+                            print('Unhandled FirebaseAuthException: ${e.code}');
+                            print(e.runtimeType);
+                            print(e);
+                          }
+                        } catch (e) {
+                          // Άλλες εξαιρέσεις
+                          print('Something bad happened...');
                           print(e.runtimeType);
                           print(e);
                         }
-                      } catch (e) {
-                        // Άλλες εξαιρέσεις
-                        print('Something bad happened...');
-                        print(e.runtimeType);
-                        print(e);
-                      }
-                    },
-                    child: const Text('Login'),
-                  ),
-
-              ],
-            );
-           default: return const Text('Loading...'); 
-          }
-          
-        },
-      ),
+                      },
+                      child: const Text('Login'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          '/register/', 
+                          (route) => false,
+                        );
+                      }, 
+                    child: const Text('Not registered yet? Register here!'),
+                  )
+                ],
+              ),
     );
   }
 }
